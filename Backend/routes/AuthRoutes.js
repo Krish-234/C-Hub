fc4e89cc409479d1f60ff90971ebf814d3951b74
Krish patel <krishp2304@gmail.com>
2024-09-13 08:33:10 +0530

    profileImage is added

--- Backend/routes/AuthRoutes.js
+++ Backend/routes/AuthRoutes.js
@@ -1,12 +1,29 @@
 import { Router } from "express";
-import { getUserInfo, login,signup } from "../controllers/Authcontrollers.js";
+import {
+  getUserInfo,
+  login,
+  signup,
+  updateProfile,
+  addProfileImage,
+  removeProfileImage,
+} from "../controllers/Authcontrollers.js";
 import { verifyToken } from "../middlewares/Authmiddleware.js";
+import multer from "multer";
+
+const upload = multer({ dest: "uploads/profiles/" });
 
 const authRoutes = Router();
 
 authRoutes.post("/signup", signup);
 authRoutes.post("/login", login);
-authRoutes.get("/user-info",verifyToken,getUserInfo);
+authRoutes.get("/user-info", verifyToken, getUserInfo);
+authRoutes.post("/update-profile", verifyToken, updateProfile);
+authRoutes.post(
+  "/add-profile-image",
+  verifyToken,
+  upload.single("profile-image"),
+  addProfileImage
+);
+authRoutes.delete("/remove-profile-image",verifyToken,removeProfileImage);
 
 export default authRoutes;
-
--- src/Container/Profile/Profile.jsx
+++ src/Container/Profile/Profile.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Profile.css";
 import { useAppStore } from "../../store";
 import { IoArrowBack } from "react-icons/io5";
 import { FaPlus, FaTrash } from "react-icons/fa"; // Importing the icons
 import { useNavigate } from "react-router-dom";
+import { apiClient } from "../../lib/api-client.js";
+import {
+  ADD_PROFILE_IMAGE_ROUTE,
+  HOST,
+  REMOVE_PROFILE_IMAGE_ROUTE,
+  UPDATE_PROFILE_ROUTE,
+} from "../../utils/constants.js";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -14,9 +21,98 @@ const Profile = () => {
   const [image, setImage] = useState(null);
   const [hovered, setHovered] = useState(false);
   const [avatarColor, setAvatarColor] = useState("#ccc"); // Default avatar color
+  const fileInputRef = useRef(null);
+
+  useEffect(() => {
+    if (userInfo.profileSetup) {
+      setFirstName(userInfo.firstName);
+      setLastName(userInfo.lastName);
+      setAvatarColor(userInfo.color);
+    }
+    if (userInfo.image) {
+      setImage(`${HOST}/${userInfo.image}`);
+    }
+  }, [userInfo]);
+
+  const validateProfile = () => {
+    if (!firstName) {
+      alert("firstName is required");
+      return false;
+    }
+    if (!lastName) {
+      alert("lastName is required");
+      return false;
+    }
+    return true;
+  };
 
   const saveChanges = async () => {
     // Logic for saving changes goes here
+    if (validateProfile()) {
+      try {
+        const response = await apiClient.post(
+          UPDATE_PROFILE_ROUTE,
+          { firstName, lastName, color: avatarColor },
+          { withCredentials: true }
+        );
+        if (response.status === 200 && response.data) {
+          setUserInfo({ ...response.data });
+          alert("profile updated successfully.");
+          navigate("/chat");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  const handleNavigate = () => {
+    if (userInfo.profileSetup) {
+      navigate("/chat");
+    } else {
+      alert("Please setup profile.");
+    }
+  };
+
+  const handleFileInputClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("profile-image", file);
+      const res = await apiClient.post(ADD_PROFILE_IMAGE_ROUTE, formData, {
+        withCredentials: true,
+      });
+
+      if (res.status === 200 && res.data.image) {
+        setUserInfo({ ...userInfo, image: res.data.image });
+        // alert("image updated successfully.");
+      }
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDeleteImage = async () => {
+    try {
+      const res = await apiClient.delete(REMOVE_PROFILE_IMAGE_ROUTE, {
+        withCredentials: true,
+      });
+      if (res.status === 200) {
+        setUserInfo({ ...userInfo, image: null });
+        // alert("image removed successfully.");
+        setImage(null);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const colorOptions = ["#FF6347", "#4682B4", "#32CD32", "#FFD700"]; // Define 4 semi-transparent colors
@@ -24,7 +120,7 @@ const Profile = () => {
   return (
     <div className="app_profile-container">
       <div className="profile-inner-container">
-        <div>
+        <div onClick={handleNavigate}>
           <IoArrowBack className="back-arrow" />
         </div>
 
@@ -44,12 +140,26 @@ const Profile = () => {
               )}
               {/* Conditionally render FaTrash if image exists, otherwise render FaPlus */}
               {hovered && (
-                <div className="icon-wrapper">
+                <div
+                  className="icon-wrapper"
+                  onClick={image ? handleDeleteImage : handleFileInputClick}
+                >
                   {image ? (
-                    <FaTrash className="avatar-icon" onClick={() => setImage(null)} />
+                    <FaTrash
+                      className="avatar-icon"
+                      onClick={() => setImage(null)}
+                    />
                   ) : (
                     <FaPlus className="avatar-icon" />
                   )}
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    className="hidden"
+                    onChange={handleImageChange}
+                    name="profile-image"
+                    accept=".png, .jpg, .jpeg, .svg, .webp"
+                  />
                 </div>
               )}
             </div>
@@ -59,7 +169,8 @@ const Profile = () => {
           <div className="profile-inputs">
             <input
               type="text"
-              disabled value={email}
+              disabled
+              value={email}
               placeholder="Email"
               className="input-field"
             />
