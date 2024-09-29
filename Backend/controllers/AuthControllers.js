f0aaf7158d9f7962d5f601bbb81dcd275a6bf18d
Krish patel <krishp2304@gmail.com>
2024-09-29 21:42:41 +0530

    same site properties removed

--- Backend/controllers/AuthControllers.js
+++ Backend/controllers/AuthControllers.js
@@ -20,9 +20,7 @@ export const signup = async (req, res, next) => {
 
     const user = await User.create({ email, password });
     res.cookie("jwt", createToken(email, user.id), {
-      maxAge,
-      secure: true,
-      sameSite: "None",
+      maxAge
     });
 
     return res.status(201).json({
@@ -55,9 +53,7 @@ export const login = async (req, res, next) => {
     }
 
     res.cookie("jwt", createToken(email, user.id), {
-      maxAge,
-      secure: true,
-      sameSite: "None",
+      maxAge
     });
 
     return res.status(200).json({
@@ -188,7 +184,7 @@ export const removeProfileImage = async (req, res, next) => {
 
 export const logout = async (req, res, next) => {
   try {
-    res.cookie("jwt", { maxAge: 1, secure: true, sameSite: "none" });
+    res.cookie("jwt", { maxAge: 1 });
     return res.status(200).send("Logout Sucessful");
   } catch (err) {
     console.log({ err });
